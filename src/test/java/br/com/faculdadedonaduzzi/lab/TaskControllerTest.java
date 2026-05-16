a758858b81e80bfd7763ff154137faae161afca2
Julio Nogueira <julio.nogueira@outlook.com>
2026-05-15 21:00:38 -0300

    Test All tarefas find

---
+++ src/test/java/br/com/faculdadedonaduzzi/lab/TaskControllerTest.java
@@ -0,0 +1,24 @@
+package br.com.faculdadedonaduzzi.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
+public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllTasks() throws Exception {
+        mockMvc.perform(get("/tarefa"))
+                .andExpect(status().isOk());
+    }
+}
